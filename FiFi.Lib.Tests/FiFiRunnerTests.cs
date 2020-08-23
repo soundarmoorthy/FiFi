5a73af26108bdee860630154cdd535c75e8d0626
Soundararajan D <dakshins@optum.com>
2020-08-23 13:39:14 +0530

    Add Tests for FiFi Runner

---
+++ FiFi.Lib.Tests/FiFiRunnerTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using FiFi;
+using Xunit;
+
+namespace FiFi.Lib.Tests
+{
+    public class FiFiRunnerTests
+    {
+        [Fact]
+        public void FiFiRunner_New_Returns_New_Session_Every_Time()
+        {
+            var expected = FiFiRunner.New();
+            var actual = FiFiRunner.New();
+
+            Assert.NotSame(expected, actual);
+        }
+    }
+}
