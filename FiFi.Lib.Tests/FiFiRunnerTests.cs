4a5060462064402679c0036375718fb1a8307c90
Soundararajan D <dakshins@optum.com>
2020-08-23 15:52:46 +0530

    Add more tests

--- FiFi.Lib.Tests/FiFiRunnerTests.cs
+++ FiFi.Lib.Tests/FiFiRunnerTests.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
 using FiFi;
 using Xunit;
 
@@ -11,8 +14,42 @@ namespace FiFi.Lib.Tests
         {
             var expected = FiFiRunner.New();
             var actual = FiFiRunner.New();
-
             Assert.NotSame(expected, actual);
+        }
+
+        [Fact]
+        public void FiFiRunner_New_Initializes_State_Properly()
+        {
+            var runner = FiFiRunner.New();
+            Assert.NotNull(runner.GetConfig());
+            Assert.NotNull(runner.GetFiles());
+            Assert.Empty(runner.GetFiles().All());
+        }
+
+        [Theory]
+        [ClassData(typeof(LineEndingDataProvider))]
+        public void FiFiRunner_FixLineEndings_Returns_Config_Correctly
+            (LineEndingMode expected)
+        {
+            var runner = FiFiRunner.New();
+            var config = runner
+                .FixLineEndings(expected)
+                .GetConfig();
+
+            Assert.Equal(expected, config.LineEnding);
+        }
+
+        [Theory]
+        [ClassData(typeof(FileEncodingDataProvider))]
+        public void FiFiRunner_FixEncoding_Returns_ConfigCorrectly
+            (Encoding expected)
+        {
+            var runner = FiFiRunner.New();
+            var config = runner
+                .FixEncoding(expected)
+                .GetConfig();
+
+            Assert.Equal(expected, config.Encoding);
         }
     }
 }
